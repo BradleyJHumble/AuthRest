3c37a041669740af9ff746653da9c6ecb110de58
Bradley's laptop <bradleyjhumble@gmail.com>
2018-09-23 17:09:25 -0500

    + User Model Export

---
+++ models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    index: true,
+    unique: true,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
